c89f69bded8f87fcac8c3eb4ec9008cfaaa10198
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-04 13:42:42 +0100

    file iniziali

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var cognomi = [
+  "Sozzi",
+  "Livio",
+  "Alivernini",
+  "Giammario",
+  "Parisi",
+  "Rubicondo",
+  "Bernini",
+  "Marino",
+  "Secondini",
+  "Sabatini",
+  "Tabbì"
+]
+
+console.log(cognomi);
+
+var cognome = prompt("Dammi il tuo cognome:");
+
+console.log(cognome);
+
+cognomi.push(cognome);
+
+console.log(cognomi);
+
+cognomi.sort();
+
+console.log(cognomi);
